09379c0442aae7212b9d2dac10feeaa92a453d6e
Lake020 <leolake2002@gmail.com>
2023-05-16 18:40:50 +0200

    Stampare i primi 3 numeri interi close #7

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -12,7 +12,26 @@ public class RomanPrinter {
     }
 
     private static String printAsciiArt(String romanNumber){
-        //TODO
-        return null;
+        String[] row ={"", "", "", "", "", ""};
+        for(int i = 0; i<romanNumber.length(); i++){
+            switch (romanNumber.charAt(i)) {
+                case 'I':
+                    row[0] = row[0] + "  _____ ";
+                    row[1] = row[1] + " |_   _|";
+                    row[3] = row[3] + "   | |  ";
+                    row[2] = row[2] + "   | |  ";
+                    row[4] = row[4] + "  _| |_ ";
+                    row[5] = row[5] + " |_____|";
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+        String s = "";
+        for(int i = 0; i < 6; i++){
+            s = s + row[i] + "\n";
+        }
+        return s;
     }
 }
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -3,10 +3,25 @@ package it.unipd.mtss;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
 public class RomanPrinterTest {
 
     @Test
-    public void test() {
-    }    
+    public void test1() {
+        try(MockedStatic<IntegerToRoman> utilities = Mockito.mockStatic(IntegerToRoman.class)){
+            utilities.when(() -> IntegerToRoman.convert(1)).thenReturn("I");
+            String exp =    "  _____ \n" + 
+                            " |_   _|\n" +
+                            "   | |  \n" +
+                            "   | |  \n" +
+                            "  _| |_ \n" +
+                            " |_____|\n";
+            String act = RomanPrinter.print(1);
+
+            assertEquals(exp, act);
+        }
+    } 
 
 }
